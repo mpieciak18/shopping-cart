4d2a549dd67c964ed2fbfd8554c8189ef26209a3
Mark Pieciak <mpieciak18@msn.com>
2021-12-03 08:49:13 -0500

    add react-dom-router & RouteSwitch.js

---
+++ src/RouteSwitch.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const RouteSwitch = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RouteSwitch
