623a51b125241407f0c4395df7eacc5c808933c5
Mark Pieciak <mpieciak18@msn.com>
2021-12-04 10:23:08 -0500

    add basic rendering for footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div>Developed by Mark Pieciak</div>
+            <a href='https://github.com/mpieciak18/shopping-cart'>
+                <img></img>
+            </a>
+        </footer>
+    )
+}
