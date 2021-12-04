df845730a3b809a4fe79fcbf6375cc467230a80c
Mark Pieciak <mpieciak18@msn.com>
2021-12-04 08:55:03 -0500

    add RouteSwitch.js file

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,10 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home.js'
+import Shop from './pages/Shop.js'
+import About from './pages/About.js'
+import ComingSoon from './pages/ComingSoon.js'
 
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
             <Routes>
-
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/home' element={<Home />} />
+                <Route exact path='/shop' element={<Shop />} />
+                <Route exact path='/about' element={<About />} />
+                <Route exact path='/coming-soon' element={<ComingSoon />} />
             </Routes>
         </BrowserRouter>
     )
