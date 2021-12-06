d24940e6a2c23e57096d8cc5602e728bf2a365c1
Mark Pieciak <mpieciak18@msn.com>
2021-12-06 10:23:07 -0500

    update header & assets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom'
-import Icon from '../assets/shopping-cart-icon.png'
+import StoreIcon from '../assets/store-icon.png'
+import CartIcon from '../assets/shopping-cart-icon.png'
 
 const Header = () => {
     return (
         <header>
             <div id='header-left'>
-                <Link to='/home'><h1 id='title'>Store Name</h1></Link>
+                <Link id='title-container' to='/home'>
+                    <h1 id='title'>StrengthStore</h1>
+                    <img id='title-icon' src={StoreIcon}></img>
+                </Link>
             </div>
             <nav id='header-right'>
                 <Link to='/home'>Home</Link>
                 <Link to='/shop'>Shop</Link>
                 <Link to='/about'>About</Link>
                 <div id='shopping-cart-container'>
-                    <img id='shopping-cart-icon' src={Icon}></img>
+                    <img id='shopping-cart-icon' src={CartIcon}></img>
                 </div>
             </nav>
         </header>
