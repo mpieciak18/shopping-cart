1757764c1b28a60136327f5e01130944084ee7ab
Mark Pieciak <mpieciak18@msn.com>
2021-12-07 17:41:31 -0500

    update home & header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,20 @@ import CartIcon from '../assets/shopping-cart-icon.png'
 const Header = () => {
     return (
         <header>
-            <div id='header-left'>
-                <Link id='title-container' to='/home'>
+            <div id='header-container'>
+                <Link id='header-left' to='/home'>
                     <h1 id='title'>StrengthStore</h1>
                     <img id='title-icon' src={StoreIcon}></img>
                 </Link>
+                <nav id='header-right'>
+                    <Link to='/home'>Home</Link>
+                    <Link to='/shop'>Shop</Link>
+                    <Link to='/about'>About</Link>
+                    <div id='shopping-cart-container'>
+                        <img id='shopping-cart-icon' src={CartIcon}></img>
+                    </div>
+                </nav>
             </div>
-            <nav id='header-right'>
-                <Link to='/home'>Home</Link>
-                <Link to='/shop'>Shop</Link>
-                <Link to='/about'>About</Link>
-                <div id='shopping-cart-container'>
-                    <img id='shopping-cart-icon' src={CartIcon}></img>
-                </div>
-            </nav>
         </header>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,17 +7,16 @@ const Home = () => {
         <div id='home' class='page'>
             <Header />
             <div id='home-contents' class='page-contents'>
-                <div id='home-contents-container'>
-                    <div id='home-contents-one'>
-                        <div id='home-contents-subheader'>#1-RATED STRENGTH EQUIPMENT SUPPLIER</div>
-                        <div id='home-contents-header'>
-                            <div id='home-contents-header-line-1'>We know strength equipment.</div>
-                            <div id='home-contents-header-line-2'>We are StrengthStore.</div>
-                        </div>
+                <div id='home-contents-one'>
+                    <div id='home-subheader'>#1-RATED STRENGTH EQUIPMENT SUPPLIER</div>
+                    <div id='home-header'>
+                        <div id='home-header-line-1'>We know strength equipment.</div>
+                        <div id='home-header-line-2'>We are StrengthStore.</div>
                     </div>
-                    <div id='home-contents-two'>
-                        <img id='home-page-picture' src={HomePicture}></img>
-                    </div>
+                    <div id='home-shop-button'>Shop Now</div>
+                </div>
+                <div id='home-contents-two'>
+                    <img id='home-picture' src={HomePicture}></img>
                 </div>
             </div>
             <Footer />
