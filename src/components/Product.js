36806a5712057b576232964dc11f7c7b73bfabe3
Mark Pieciak <mpieciak18@msn.com>
2021-12-08 11:10:39 -0500

    create basic product JSX structure

---
+++ src/components/Product.js
@@ -0,0 +1,16 @@
+const Product = () => {
+    return (
+        <div class='product'>
+            <div class='product-top-half'>
+                <img class='product-image' src='' />
+            </div>
+            <div class='product-bottom-half'>
+                <div class='product-title'></div>
+                <div class='product-price'></div>
+                <div class='product-button'></div>
+            </div>
+        </div>
+    )
+}
+
+export default Product
