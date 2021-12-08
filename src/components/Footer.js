4f8324b4d74fea070e53171cee34c081b24be033
Mark Pieciak <mpieciak18@msn.com>
2021-12-07 21:11:10 -0500

    fix footer hyperlink decoration

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,10 @@ import GithubLogo from '../assets/github-logo.png'
 
 const Footer = () => {
     return (
-        <footer>
-            <div>Developed by Mark Pieciak</div>
-            <a href='https://github.com/mpieciak18/shopping-cart' target='_blank'>
-                <img id='github-logo' src={GithubLogo}></img>
-            </a>
-        </footer>
+        <a href='https://github.com/mpieciak18/shopping-cart' target='_blank' id='footer'>
+            <div id='footer-text'>Developed by Mark Pieciak</div>
+            <img id='github-logo' src={GithubLogo}></img>
+        </a>
     )
 }
 
