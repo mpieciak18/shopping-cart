9de83a9ce7a6356c8f383040bd4d77530aa429a2
Mark Pieciak <mpieciak18@msn.com>
2021-12-09 08:26:14 -0500

    reconsolidate state

--- src/App.js
+++ src/App.js
@@ -13,16 +13,14 @@ import Bench from './assets/images/bench.png'
 
 const App = () => {
     const initProductState = [
-        {title: 'Squat Rack', price: 499, image: SquatRack},
-        {title: 'Power Rack', price: 799, image: PowerRack},
-        {title: '45lb Olympic Barbell', price: 299, image: Barbell},
-        {title: '45lb Iron Plates - Pair', price: 59, image: IronPlates},
-        {title: '45lb Bumper Plates - Pair', price: 119, image: BumperPlates},
-        {title: 'Bench', price: 199, image: Bench},
+        {id: 0, title: 'Squat Rack', price: 499, image: SquatRack, quantity: 0},
+        {id: 1, title: 'Power Rack', price: 799, image: PowerRack, quantity: 0},
+        {id: 2, title: '45lb Olympic Barbell', price: 299, image: Barbell, quantity: 0},
+        {id: 3, title: '45lb Iron Plates - Pair', price: 59, image: IronPlates, quantity: 0},
+        {id: 4, title: '45lb Bumper Plates - Pair', price: 119, image: BumperPlates, quantity: 0},
+        {id: 5, title: 'Bench', price: 199, image: Bench, quantity: 0},
     ]
     const [products, setProducts] = useState(initProductState)
-
-    const [cart, setCart] = useState([])
 
     const [total, setTotal] = useState(0)
 
@@ -31,7 +29,7 @@ const App = () => {
             <Routes>
                 <Route exact path='/' element={<Home />} />
                 <Route exact path='/home' element={<Home />} />
-                <Route exact path='/shop' element={<Shop products={products} cart={cart} setCart={setCart}/> } />
+                <Route exact path='/shop' element={<Shop products={products}/> } />
                 <Route exact path='/about' element={<About />} />
                 <Route exact path='/coming-soon' element={<ComingSoon />} />
             </Routes>
