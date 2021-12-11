567b220dd8cc2bbf9fc2e2361f53cadb341b121c
Mark Pieciak <mpieciak18@msn.com>
2021-12-11 13:52:30 -0500

    display cart total cost

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,12 +1,12 @@
 const ShoppingCart = (props) => {
-    const { visibility, toggleVisibility } = props
+    const { visibility, toggleVisibility, total } = props
 
     return (
         <div id='shopping-cart' class={visibility}>
             <div id='shopping-cart-container'>
                 <div id='shopping-cart-title'>Your Shopping Cart</div>
                 <div id='shopping-cart-items-container'>(ITEMS: PLACEHOLDER)</div>
-                <div id='shopping-cart-total'>(TOTAL: PLACEHOLDER)</div>
+                <div id='shopping-cart-total'>${total}.00</div>
                 <div id='shopping-cart-buttons'>
                     <div id='shopping-cart-checkout'>Checkout</div>
                     <div id='shopping-cart-close' onClick={toggleVisibility}>Close</div>
