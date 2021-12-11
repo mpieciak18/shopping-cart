2fbcaa0a6c2c62f24fc8365fe3812519c62c8b24
Mark Pieciak <mpieciak18@msn.com>
2021-12-11 15:28:01 -0500

    style shopping cart items container

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -14,12 +14,35 @@ const ShoppingCart = (props) => {
         setTotal(newTotal)
     }, [cart])
 
+    const items = (
+        <div id='shopping-cart-items-container'>
+            {cart.map((item) => {
+                    return (
+                        <div id={item.id} class='shopping-cart-item'>
+                            <div class='shopping-cart-item-left'>
+                                <img class='shopping-cart-item-image' src={item.image} />
+                            </div>
+                            <div class='shopping-cart-item-right'>
+                                <div class='shopping-cart-item-title'>{item.title}</div>
+                                <div class='shopping-cart-item-subtotal'>${item.subtotal}.00</div>
+                                <div class='shopping-cart-item-quantity-container'>
+                                    <div class='shopping-cart-item-subtract-button'>-</div>
+                                    <div class='shopping-cart-item-quantity'>{item.quantity}</div>
+                                    <div class='shopping-cart-item-add-button'>+</div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+        </div>
+    )
+
     return (
         <div id='shopping-cart' class={visibility}>
             <div id='shopping-cart-container'>
                 <div id='shopping-cart-title'>Your Shopping Cart</div>
-                <div id='shopping-cart-items-container'>(ITEMS: PLACEHOLDER)</div>
-                <div id='shopping-cart-total'>${total}.00</div>
+                {items}
+                <div id='shopping-cart-total'>Total: ${total}.00</div>
                 <div id='shopping-cart-buttons'>
                     <div id='shopping-cart-checkout'>Checkout</div>
                     <div id='shopping-cart-close' onClick={toggleVisibility}>Close</div>
